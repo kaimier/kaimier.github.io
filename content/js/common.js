4b3bb78e84be29b2892ba32d5393e73935092d27
suzhihui <wisdom.su@163.com>
2015-10-14 16:30:03 +0800

    [动态头部js调用]

---
+++ content/js/common.js
@@ -0,0 +1,7 @@
+var temple=		'<a href="/" class="active">首页</a>'+
+				'<a href="/kaimier.github.io">开米尔站</a>'+
+				'<a href="/suzhihui.github.io">老苏博客</a>'+
+				'<a href="#">联系方式</a>';
+$(function(){
+	$('.commonheader nav').html(temple);
+});
