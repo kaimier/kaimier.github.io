64d9af81a8ac367c40677f6f525b27b486bd4b2e
suzhihui <wisdom.su@163.com>
2015-11-03 17:20:20 +0800

    【php -mysqli】

---
+++ php/test02/mySqli/addUser.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>添加用户</title>
+</head>
+<body>
+    <h2>添加用户</h2>
+    <form action="doAction.php?act=addUser" method="post">
+        <table border='1' cellspacing="0" cellpadding="0" bgcolor="#abcdef" width="80%">
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
