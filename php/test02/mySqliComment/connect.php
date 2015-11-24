b32d1124bd6e8e5bfee65607af14f20a0c2428d5
suzhihui <wisdom.su@163.com>
2015-11-24 08:48:41 +0800

    delete mySqliComment

--- php/test02/mySqliComment/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $mysqli=new mysqli('localhost','root','123456','imoocComment');
-    if ($mysqli->errno) {
-        die('Connect error:'.$mysqli->error);
-    }else{
-        $mysqli->set_charset('utf8');
-    }
- ?>
