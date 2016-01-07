beb7d7535fffa87b5de1b29ce805f215cfa91fc7
suzhihui <wisdom.su@163.com>
2016-01-07 22:32:28 +0800

    xxx

---
+++ donet/note2012/note01/_05面向对象1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05面向对象1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            xxx
+        }
+    }
+}
