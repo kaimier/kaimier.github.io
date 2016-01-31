1e34570b15e3bd7340c676176d1d8f399b5557ca
suzhihui <wisdom.su@163.com>
2016-01-31 09:28:42 +0800

    学习两句话

--- donet-sukun/_01我的第一个项目/_01我的第一个项目/Program.cs
+++ donet-sukun/_01我的第一个项目/_01我的第一个项目/Program.cs
@@ -8,8 +8,15 @@ namespace _01我的第一个项目
 {
     class Program
     {
+        /// <summary>
+        /// .sln解决方案的文件
+        /// .csproj 项目文件
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello,World!");
+            Console.Read();
         }
     }
 }
