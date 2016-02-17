609a9e1b59c5db6bbcdafef27ccea1c198f0ccf1
suzhihui <wisdom.su@163.com>
2016-02-17 17:22:36 +0800

    [9zhou]

--- 九洲聚才/js/common.js
+++ 九洲聚才/js/common.js
@@ -10,3 +10,12 @@
         });
     });
 })('老苏');
+;(function() {
+    $('.e_btns span').each(function(i,el) {
+        $(el).click(function() {
+            $(el).addClass('active').siblings().removeClass('active');
+            $('.e_tabbox').hide();
+            $('.e_tabbox').eq(i).show();
+        })
+    })
+})('ebay页面tab切换');
