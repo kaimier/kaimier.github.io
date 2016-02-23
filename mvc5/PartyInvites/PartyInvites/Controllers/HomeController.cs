cd8b51531c880ee3557bf7ab8abbae6038730741
suzhihui <wisdom.su@163.com>
2016-02-23 23:46:11 +0800

    mvc5

---
+++ mvc5/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //ToDo:对晚会的组织者发送Email响应
+            return View("Thanks", guestResponse);
+        }
+	}
+}
