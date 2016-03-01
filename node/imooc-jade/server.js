f8ead068186be35a4b5a8914a7c8cec176e0af11
suzhihui <wisdom.su@163.com>
2016-03-01 17:31:34 +0800

    jade

---
+++ node/imooc-jade/server.js
@@ -0,0 +1,24 @@
+var http = require('http')
+var jade = require('jade')
+
+http.createServer(function(req,res) {
+    res.writeHead(200,{
+        'Content-Type':'text/html '
+    })
+    //1.compile
+    // var fn = jade.compile('div #{course}',{})
+    // var html = fn({course:'jade'})
+
+    //2. jade render
+    // var html = jade.render('div #{course}',{course:'jade render'})
+ 
+    //3. jade.renderFile
+    var html = jade.renderFile('index.jade',{ course:'jade renderFile',pretty:true})
+
+
+
+
+    res.end(html)
+
+}).listen(1337,'127.0.0.1')
+console.log('Server running at 1337');
