2fb729878744eb6e0994c03549ed9f21b0824d9e
suzhihui <wisdom.su@163.com>
2016-03-02 17:07:47 +0800

    imooc-jade/imooc.jade

--- node/imooc/app.js
+++ node/imooc/app.js
@@ -1,60 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+var express = require('express')
+var port = process.env.PORT || 3000
+var app = express()
+app.set('views','./views')
+app.set('view engine', 'jade')
+app.listen(port)
+console.log('imooc started on port ' + port);
+//index page
+app.get('/',function(req,res) {
+    res.render('index',{
+        title:'imooc 首页'
+    })
+})
+//detail page
+app.get('/movie/:id',function(req,res) {
+    res.render('deatail',{
+        title:'imooc 详情页'
+    })
+})
+//admin page
+app.get('/admin/movie',function(req,res) {
+    res.render('admin',{
+        title:'imooc 后面录入页'
+    })
+})
+//list page
+app.get('/admin/list',function(req,ress) {
+    res.render('list',{
+        title:'imooc 列表页'
+    })
+})
